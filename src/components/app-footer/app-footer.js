5d856e6ab79600ade53736b346b33b6bab0652c7
denis <you@example.com>
2021-01-26 09:34:18 +0200

    fix warning

--- src/components/app-footer/app-footer.js
+++ src/components/app-footer/app-footer.js
@@ -35,7 +35,7 @@ export default AppFooter;
 const GoogleMap = () => {
   return (
       <div className='map'>
-        <iframe
+        <iframe title="map"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2721.0066197532196!2d28.85774461560969!
               3d47.00084297914915!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40c97ea9e046c4d3%3A0x13278ab505741
               c64!2sBulevardul%20Decebal%20139%2C%20Chi%C8%99in%C4%83u!5e0!3m2!1sru!2s!4v1611586748666!5m2!1sru!2s"
