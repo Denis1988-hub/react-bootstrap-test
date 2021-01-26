5b6a11353995a2a2eeebe9d09b327db3fbc032f8
denis <you@example.com>
2021-01-26 09:12:52 +0200

    change some images

--- src/components/app-portfolio/app-portfolio.js
+++ src/components/app-portfolio/app-portfolio.js
@@ -2,8 +2,19 @@ import React from 'react';
 import {Card, Container, Row, Nav} from 'react-bootstrap';
 import './app-portfolio.css';
 import Tab from "react-bootstrap/Tab";
-import {portfolio} from '../../img/img'
+import {p1,p2,p3,p4,p5,p6,p7,p8} from '../../img/img'
 
+const portfolio = [
+  {image: p1, link: 'https://www.ipvanish.com/'},
+  {image: p2, link: 'https://azm.to/'},
+  {image: p3, link: 'https://burst.shopify.com/home'},
+  {image: p4, link: 'https://www.popcornflix.com/pages/discover/d/tv-shows'},
+  {image: p5, link: 'https://www.myheritage.ro/'},
+  {image: p6, link: 'https://www.demokrathaber1.net/tag/bailey-cars/'},
+  {image: p7, link: 'https://seolitte.com/'},
+  {image: p8, link: 'https://mmpcfl.com/covid-19-branded-face-masks-hand-sanitizer-safety-signs-and-more-main-page/'},
+
+];
 
 const AppPortfolio = () => {
 
@@ -12,7 +23,7 @@ const AppPortfolio = () => {
       return (
           <>
           <Nav.Link className="render-link" href={item.link} target="_blank">
-            <Card className={'card-photo mt-5'} key={index + 'AAA'}>
+            <Card className={'card-photo mt-5'} key={index + item.link}>
               <Card.Img variant="top" src={item.image}/>
             </Card>
           </Nav.Link>
--- src/img/img.js
+++ src/img/img.js
@@ -13,26 +13,20 @@ import p7 from './portfolio/p7.png';
 import p8 from './portfolio/p8.png';
 
 
-const portfolio = [
-  {image: p1, link: 'https://www.ipvanish.com/'},
-  {image: p2, link: 'https://azm.to/'},
-  {image: p3, link: 'https://burst.shopify.com/home'},
-  {image: p4, link: 'https://www.popcornflix.com/pages/discover/d/tv-shows'},
-  {image: p5, link: 'https://www.myheritage.ro/'},
-  {image: p6, link: 'https://www.demokrathaber1.net/tag/bailey-cars/'},
-  {image: p7, link: 'https://seolitte.com/'},
-  {image: p8, link: 'https://mmpcfl.com/covid-19-branded-face-masks-hand-sanitizer-safety-signs-and-more-main-page/'},
-
-];
-
-
 export {
   logo,
     pic,
     s1,
     s2,
     s3,
-   portfolio
+   p1,
+   p2,
+   p3,
+   p4,
+   p5,
+   p6,
+   p7,
+   p8,
 }
 
 
